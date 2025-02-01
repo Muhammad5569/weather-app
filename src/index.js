044b13e70fb6dc70ba36b066b13bd6b297210c56
Muhammad <117420619+Muhammad5569@users.noreply.github.com>
2025-02-01 06:21:06 +0000

    fixed

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRouter = require("./routers/user");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/user-api");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
